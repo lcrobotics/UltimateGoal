f41b7c9e648b3974d400fc7d72494ac29c220d2e
Ty Ma <tyimamahuhu@gmail.com>
2020-12-17 17:21:29 -0500

    comment teleop class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -10,6 +10,7 @@ import java.util.Set;
 
 @TeleOp
 public class Teleop extends OpMode {
+    // Defines each piece of hardware
     DcMotor intake;
     DcMotor linearActuator;
     DcMotor shooter;
@@ -18,47 +19,55 @@ public class Teleop extends OpMode {
 
     boolean grabservo = true;
     boolean pushservo = true;
+
     @Override
     public void init() {
+        // Initializes each piece of hardware
         intake = hardwareMap.get(DcMotor.class, "intake");
         linearActuator = hardwareMap.get(DcMotor.class, "linearActuator");
         shooter = hardwareMap.get(DcMotor.class, "shooter");
         openGrabber = hardwareMap.get(Servo.class, "openGrabber");
         push = hardwareMap.get(Servo.class, "push");
-        //Identifies
     }
 
     @Override
     public void loop() {
+        // calls all methods
         grabbing();
         shooter();
         push();
         intake();
     }
 
-
+    // Code for the grabber. Servo grabs the ring and the linear actuator lifts it up.
     public void grabbing() {
+        // set motor power to 1 when right bumper is pressed
         if (gamepad1.right_bumper) {
             linearActuator.setPower(1);
         }
+        // starts servo when b is pressed
         if (gamepad1.b) {
             grabservo = true;
         }
     }
-
+    // code for the ring shooter which shoots the rings.
     public void shooter() {
+        // sets power to 1 when left stick button s pressed
         if (gamepad1.left_stick_button) {
             shooter.setPower(1);
         }
     }
-
+    // code for the push servo that pushes the rings into the shooter.
     public void push() {
+        // starts servo when a is pressed
         if (gamepad1.a) {
             pushservo = true;
         }
     }
 
+    // code for the intake motor which brings the rings into the robot.
     public void intake() {
+        // sets power to 1 when left bumper is pressed
         if (gamepad1.left_bumper) {
             intake.setPower(1);
         }
