ff3eb51937b8e31fccd3d5ea1e6fc9387ca02b28
chloezp <71338303+chloezp@users.noreply.github.com>
2021-02-12 15:55:49 -0500

    skeleton for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/Autonomous.java
@@ -1,4 +1,70 @@
 package org.firstinspires.ftc.teamcode.AutonomousOpModes;
 
-public class Autonomous {
+import org.firstinspires.ftc.teamcode.SuperOp;
+
+public class Autonomous extends SuperOp {
+
+    int auto = 0;
+
+    final int DRIVE = 0;
+    final int CLOCKWISE = 1;
+    final int DELTA = 2;
+    final int COUNTERCLOCKWISE = 3;
+    final int SHOOT = 4;
+
+    boolean picture;
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    public void loop() {
+        //drive forward
+        switch (auto) {
+
+            case DRIVE:
+                //drive forward
+                auto = CLOCKWISE;
+                break;
+
+            case CLOCKWISE:
+                //Clockwise rotation
+
+                if (picture) {
+                    auto = DELTA;
+                } else {
+                    auto = COUNTERCLOCKWISE;
+                }
+                break;
+
+            case DELTA:
+                //find traslational delta
+
+                //find rotational delta
+
+                //if deltas in threshold, go to SHOOT
+
+                //if not, go to DELTA
+                break;
+
+            case COUNTERCLOCKWISE:
+                //Counterclockwise rotation
+
+                if (picture) {
+                    auto = DELTA;
+                } else {
+                    auto = CLOCKWISE;
+                }
+                break;
+
+            case SHOOT:
+                //shoot
+                break;
+        }
+    }
+
+    public boolean pictureFound() {
+            return false;
+    }
 }
