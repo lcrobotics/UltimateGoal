725f3a105b8072f8b53fb2bfbc78b9d5f5fc073b
Alexa <alexa_becker@loomis.org>
2021-02-12 16:17:21 -0500

    switch superOp over to using EasyFC

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SuperOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SuperOp.java
@@ -1,36 +1,38 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.lcrobotics.easyftclib.CommandCenter.hardware.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public abstract class SuperOp extends OpMode {
+    final int cpr = 448;
+    final int rpm = 64;
     //motors/servos for exactly what it says
-    DcMotor Intake;
-    DcMotor Rotator;
-    DcMotor Shooter;
+    Motor Intake;
+    Motor Rotator;
+    Motor Shooter;
     Servo FrontHook;
     Servo TopHook;
     Servo ShooterServo;
     //wheels
-    DcMotor FrontLeftDrive;
-    DcMotor BackLeftDrive;
-    DcMotor FrontRightDrive;
-    DcMotor BackRightDrive;
+    Motor FrontLeftDrive;
+    Motor BackLeftDrive;
+    Motor FrontRightDrive;
+    Motor BackRightDrive;
 
     @Override
     public void init() {
         // Initializes each piece of hardware
-        Intake = hardwareMap.get(DcMotor.class, "Intake");
-        Rotator = hardwareMap.get(DcMotor.class, "Rotator");
-        Shooter = hardwareMap.get(DcMotor.class, "Shooter");
+        Intake = new Motor(hardwareMap, "Intake", cpr, rpm);
+        Rotator = new Motor(hardwareMap, "Rotator", cpr, rpm);
+        Shooter = new Motor(hardwareMap, "Shooter", cpr, rpm);
         FrontHook = hardwareMap.get(Servo.class, "FrontHook");
         TopHook = hardwareMap.get(Servo.class, "TopHook");
         ShooterServo = hardwareMap.get(Servo.class, "ShooterServo");
 
-        FrontLeftDrive = hardwareMap.get(DcMotor.class, "FrontLeftDrive");
-        BackLeftDrive = hardwareMap.get(DcMotor.class, "BackLeftDrive");
-        FrontRightDrive = hardwareMap.get(DcMotor.class, "BackRightDrive");
-        BackRightDrive = hardwareMap.get(DcMotor.class, "BackRightDrive");
+        FrontLeftDrive = new Motor(hardwareMap, "FrontLeftDrive", cpr, rpm);
+        BackLeftDrive = new Motor(hardwareMap, "BackLeftDrive", cpr, rpm);
+        FrontRightDrive = new Motor(hardwareMap, "FrontRightDrive", cpr, rpm);
+        BackRightDrive = new Motor(hardwareMap, "BackRightDrive", cpr, rpm);
     }
 }
