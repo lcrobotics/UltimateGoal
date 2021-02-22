0514b458ecd2bdbfafd14a88f3841e2a0ff28434
chloezp <71338303+chloezp@users.noreply.github.com>
2021-02-22 16:27:18 -0500

    add comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/FirstAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/FirstAuto.java
@@ -55,6 +55,8 @@ public class FirstAuto extends SuperOp {
                 }
 
                 drive.driveRobotCentric(0, 0.5, 0);
+                // once robot drives for >3 secs, goes to clockwise case
+                // resets lock
                 if (time.seconds() >= 3) {
                     lock = false;
                     auto = AutoState.CLOCKWISE;
@@ -71,6 +73,8 @@ public class FirstAuto extends SuperOp {
 
                 // select time for rotation based on the rotNum we are on
                 int rotTime = 4;
+                // if number of rotation = 0 or 4, rotation time = 2 seconds
+                // if not, rotation time = 4 seconds
                 if (rotNum == 0 || rotNum == 4) {
                     rotTime = 2;
                 }
@@ -82,10 +86,12 @@ public class FirstAuto extends SuperOp {
                 if (objectLocator.targetVisible) {
                     lock = false;
                     auto = AutoState.ANGLE;
+                 // if time is < rotation time, reset lock and go to FAIL
                 } else if (time.seconds() >= rotTime) {
                     lock = false;
                     if (rotNum == 4) {
                         auto = AutoState.FAIL;
+                    // if target is not visible, go to counterclockwise
                     } else {
                         auto = AutoState.COUNTERCLOCKWISE;
                         rotNum++;
@@ -103,10 +109,14 @@ public class FirstAuto extends SuperOp {
 
                 drive.driveRobotCentric(0, 0, -0.5);
 
+                // attempt to get robot's location based on nav target
                 objectLocator.updateRobotLocation();
+
+                // if the target is visible, go to angle case
                 if (objectLocator.targetVisible) {
                     lock = false;
                     auto = AutoState.ANGLE;
+                // if the robot has not seen picture in 4 secs, go back to clockwise to try again
                 } else if (time.seconds() >= 4) {
                     lock = false;
                     rotNum++;
@@ -114,15 +124,19 @@ public class FirstAuto extends SuperOp {
                 }
                 break;
 
-                // get robot's position based on nav target and update angle until the robot is facing the nav target directly
+                // get robot's position based on nav target and update angle
+            // until the robot is facing the nav target directly
             case ANGLE:
 
+                // optimal angle the robot should be facing
                 lastPos = objectLocator.lastPos;
 
+                // adjust position until angle is within pre-decided threshold
                 if (lastPos.w > 92) {
                     drive.driveRobotCentric(0, 0, 0.5);
                 } else if (lastPos.w < 88) {
                     drive.driveRobotCentric(0, 0, -0.5);
+                // if angle is within threshold, go to sideways case
                 } else {
                     auto = AutoState.SIDEWAYS;
                 }
@@ -137,10 +151,12 @@ public class FirstAuto extends SuperOp {
 
                 lastPos = objectLocator.lastPos;
 
+                // adjust position until robot pos is within a pre-decided threshold
                 if (lastPos.y > desiredY + 2) {
                     drive.driveRobotCentric(0.5, 0, 0);
                 } else if (lastPos.y < desiredY - 2) {
                     drive.driveRobotCentric(-0.5, 0, 0);
+                // when robot within threshold, go to back case
                 } else {
                     auto = AutoState.BACK;
                 }
@@ -153,10 +169,12 @@ public class FirstAuto extends SuperOp {
 
                 lastPos = objectLocator.lastPos;
 
+                // adjust robot position based on pre-determined values
                 if (lastPos.x > desiredX + 2) {
                     drive.driveRobotCentric(0, -0.5, 0);
                 } else if (lastPos.x < desiredX - 2) {
                     drive.driveRobotCentric(0, 0.5, 0);
+                // if robot in right place, go to shoot case
                 } else {
                     auto = AutoState.SHOOT;
                 }
@@ -165,24 +183,34 @@ public class FirstAuto extends SuperOp {
             case SHOOT:
 
                 if (!lock) {
+                    // sets shooter motor to 1, will run for rest of case
                     shooter.set(1);
                     time.reset();
                     lock = true;
+                    target = true;
                 }
 
-                shooterServo.setPosition(target? 1 : 0);
 
                 if (time.seconds() > 0.5) {
+
+                    // set servo to 0 = second half of shooting action
                     shooterServo.setPosition(target? 0 : 1);
                     time.reset();
+                    target = !target;
                     ringsShot++;
+
+                } else {
+                    // set servo to 1 = first half of shooting action
+                    shooterServo.setPosition(target? 1 : 0);
                 }
-                if (ringsShot == 3) {
+                // caps the number of shots at 3
+                if (ringsShot == 6) {
                     lock = false;
                     auto = AutoState.FAIL;
                 }
                 break;
 
+                // stops OpMode
             case FAIL:
                 requestOpModeStop();
                 break;
