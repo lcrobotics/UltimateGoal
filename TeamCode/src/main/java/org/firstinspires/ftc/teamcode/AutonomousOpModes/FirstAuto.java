6fe2af2b08b9531797e4be23bb0e85d3ab34a251
Sean Lee <seanlee@Seans-MBP-2.loomis.org>
2021-02-25 17:30:04 -0500

    almost working auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/FirstAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/FirstAuto.java
@@ -13,10 +13,11 @@ public class FirstAuto extends AutoSuperOp {
     ObjectLocator.RobotPos lastPos;
     ElapsedTime time;
     boolean target;
+    boolean strafeAngle = false;
     boolean lock = false;
     int ringsShot = 0;
     int rot = 0; // 0 when adjusting angle the first time, 1 when adjusting angle the second time
-    int checkState = 0; // 0 when checking for target during rotation, 1 when strafing, 2 when going back
+    int checkState = 0; // 0 when checking for target during rotation, 1 when angle adjusting, 2 when strafing, 3 when going back
     @Override
     public void init() {
         super.init();
@@ -61,10 +62,22 @@ public class FirstAuto extends AutoSuperOp {
                     objectLocator.updateRobotLocation();
                     telemetry.addData("VISIBLE", objectLocator.targetVisible);
 
-                    if (checkState == 1) {
-                        auto = AutoState.SIDEWAYS;
+                    if (strafeAngle) {
                         lastPos = objectLocator.lastPos;
                         lock = false;
+                        auto = AutoState.ANGLE;
+                        break;
+                    }
+
+                    if (checkState > 0) {
+                        lastPos = objectLocator.lastPos;
+                        lock = false;
+                    }
+                    if (checkState == 1) {
+                        auto = AutoState.ANGLE;
+                        break;
+                    } else if (checkState == 2) {
+                        auto = AutoState.SIDEWAYS;
                         break;
                     }
 
@@ -131,6 +144,19 @@ public class FirstAuto extends AutoSuperOp {
             // until the robot is facing the nav target directly
             case ANGLE:
 
+                if (!lock) {
+                    time.reset();
+                    lock = true;
+                }
+
+                if (time.seconds() > 0.1) {
+                    lock = false;
+                    if (checkState == 0) {
+                        checkState = 1;
+                    }
+                    auto = AutoState.STOPCHECK;
+                }
+
                 // optimal angle the robot should be facing
                 objectLocator.updateRobotLocation();
                 lastPos = objectLocator.lastPos;
@@ -145,6 +171,10 @@ public class FirstAuto extends AutoSuperOp {
                 // if angle is within threshold, go to sideways case
                 } else {
                     drive.stop();
+                    lock = false;
+                    if (strafeAngle) {
+                        auto = AutoState.SIDEWAYS;
+                    }
                     if (rot == 0) {
                         auto = AutoState.SIDEWAYS;
                     } else {
@@ -158,6 +188,8 @@ public class FirstAuto extends AutoSuperOp {
                 // finds translational delta (only R/L) and adjusts to within threshold.
             case SIDEWAYS:
 
+                strafeAngle = true;
+
                 // get robot's position and update sideways position
                 double desiredY = 33;
 
@@ -169,23 +201,22 @@ public class FirstAuto extends AutoSuperOp {
                 // adjust position until robot pos is within a pre-decided threshold
                 if (lastPos.y > desiredY + 2) {
                     drive.driveRobotCentric(0.4, 0, 0);
-                    telemetry.addData("where am i", "left");
                 } else if (lastPos.y < desiredY - 2) {
                     drive.driveRobotCentric(-0.4, 0, 0);
-                    telemetry.addData("where am i", "right");
                     // when robot within threshold, go to back case
                 } else {
+                    strafeAngle = false;
                     drive.stop();
-                    telemetry.addData("where am i", "done");
                     lock = false;
                     auto = AutoState.BACK;
                     break;
                 }
 
-                if (time.seconds() > 0.2) {
+                // 0.2
+                if (time.seconds() > 0.35) {
                     lock = false;
-                    if (checkState == 0) {
-                        checkState = 1;
+                    if (checkState == 1) {
+                        checkState = 2;
                     }
                     auto = AutoState.STOPCHECK;
                 }
@@ -224,8 +255,9 @@ public class FirstAuto extends AutoSuperOp {
                 }
 
                 drive.driveRobotCentric(0, 0.3, 0);
+
                 // resets lock
-                if (time.seconds() >= 0.7) {
+                if (time.seconds() >= 3) {
                     lock = false;
                     drive.stop();
                     rot++;
@@ -245,7 +277,7 @@ public class FirstAuto extends AutoSuperOp {
                 }
 
 
-                if (time.seconds() > 0.5) {
+                if (time.seconds() > 0.7) {
 
                     // set servo to 0 = second half of shooting action
                     shooterServo.setPosition(target? 0 : 1);
@@ -270,9 +302,11 @@ public class FirstAuto extends AutoSuperOp {
                     lock = true;
                 }
 
-                drive.driveRobotCentric(0, -0.3, 0);
+                if (time.seconds() >= 1) {
+                    drive.driveRobotCentric(0, -0.3, 0);
+                }
 
-                if (time.seconds() >= 0.5) {
+                if (time.seconds() >= 2.5) {
                     lock = false;
                     drive.stop();
                     auto = AutoState.FAIL;
