9b1bf5b01eb12ed2f78407f57e310bb03cc9ab52
Alexa Becker <alexa_becker@loomis.org>
2021-05-06 15:45:46 -0400

    have ShooterTest start the spin motor before the shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpModes/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpModes/ShooterTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.TeleopOpModes;
 import com.lcrobotics.easyftclib.commandCenter.hardware.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
 // extends OpMode instead of SuperOp because the prototype doesn't have any of the other motors
@@ -15,10 +16,8 @@ public class ShooterTest extends OpMode {
     Motor spin;
     Motor shoot;
 
-    // shooter booleans (for toggle)
-    boolean shooterOn = false;
-    boolean isLB = false;
-    boolean wasLB = false;
+    // declare elapsed time constructor
+    ElapsedTime time;
 
     // initialize hardware and constructors
     public void init() {
@@ -30,6 +29,9 @@ public class ShooterTest extends OpMode {
         // set zero power to float instead of brake so the motors don't burn out trying to stop
         spin.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
         shoot.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
+
+        // initialize time constructor
+        time = new ElapsedTime();
     }
 
    // run TeleOp methods (in this case, just newShooter())
@@ -38,6 +40,10 @@ public class ShooterTest extends OpMode {
         newShooter();
    }
 
+    // shooter booleans (for toggle)
+    boolean shooterOn = false;
+    boolean isLB = false;
+    boolean wasLB = false;
     // run both motors on driver's left bumper press
     public void newShooter() {
         // track history of button
@@ -47,9 +53,14 @@ public class ShooterTest extends OpMode {
                 shoot.set(0);
                 spin.set(0);
             } else {
-                // if the shooter is off and left bumper is pressed, turn shoot and spin off
+                // if the shooter is off and left bumper is pressed, turn shoot on
                 shoot.set(1);
-                spin.set(1);
+                // give the shoot motor a second to spin up before putting any rings in
+                // the spin motor and intake turn on after a second of the shooter running
+                // this allows shoot to get up to full speed
+                if (time.milliseconds() >= 1000) {
+                    spin.set(1);
+                }
             }
             shooterOn = !shooterOn;
         }
