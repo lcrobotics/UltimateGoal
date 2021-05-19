f1766904c9dcad9f58a19548e6590633f2a4672e
Alexa Becker <alexa_becker@loomis.org>
2021-05-19 16:41:46 -0400

    add capability for powershots (not fully working) and continue work on indexing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpModes/ShooterIntakeSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpModes/ShooterIntakeSensor.java
@@ -4,7 +4,6 @@ import com.lcrobotics.easyftclib.commandCenter.hardware.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
@@ -14,10 +13,13 @@ public class ShooterIntakeSensor extends OpMode {
     final double CAROUSEL_ACTIVATION_TIME = 1000.0;
     // constant used for the shooter power
     final double SHOOTER_POWER = 1;
-    // constant used for the carousel's power
-    final double CAROUSEL_POWER = .9;
+    // constant used for the carousel's power while shooting
+    final double CAROUSEL_SHOOTING_POWER = 1;
     // constant used for the intake's power
     final double INTAKE_POWER = 1;
+    // constant used for the carousel's power while indexing
+    final double CAROUSEL_INDEXING_POWER = .4;
+    // threshold used for deadzones on joysticks and making the triggers into buttons
     final double THRESHOLD = .12;
 
     // declare motor constants
@@ -31,8 +33,6 @@ public class ShooterIntakeSensor extends OpMode {
 
     // declare new ElapsedTime (needed for shooter)
     ElapsedTime time;
-    // declare new NormalizedColorSensor (needed to index rings)
-    //NormalizedColorSensor colorSensorNormalized;
     // declare new ColorSensor (needed to index rings)
     ColorSensor colorSensor;
 
@@ -47,9 +47,6 @@ public class ShooterIntakeSensor extends OpMode {
 
         // initialize time constructor
         time = new ElapsedTime();
-
-        // initialize NormalizedColorSensor
-        //colorSensorNormalized = hardwareMap.get(NormalizedColorSensor.class, "sensor_color");
         // initialize ColorSensor
         colorSensor = hardwareMap.get(ColorSensor.class, "sensor_color");
     }
@@ -66,6 +63,8 @@ public class ShooterIntakeSensor extends OpMode {
         telemetry.addData("notRing", notRing);
         telemetry.addData("ring count", ringCount);
         telemetry.addData("carousel on?", isCarouselRunning);
+        telemetry.addData("time", time.milliseconds());
+        telemetry.addData("wait time", carouselActivationWaitTime);
     }
 
 
@@ -77,8 +76,11 @@ public class ShooterIntakeSensor extends OpMode {
     boolean prevLB = false;
     // ensure that the carousel's wait time is only set once per toggle
     boolean isCarouselWaitSet = false;
+    boolean isCarouselManuallyStopped = false;
     // run shoot on driver's left bumper press and run spin a second later
     public void newShooter() {
+        carousel.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
+        isCarouselManuallyStopped = false;
         // check if the driver's left bumper is pressed and if it's previous value is false (prevLB)
         // if both conditions are met, toggle boolean isShooterRunning, causing the shooter to either
         // turn on or off
@@ -101,23 +103,33 @@ public class ShooterIntakeSensor extends OpMode {
             shoot.set(SHOOTER_POWER);
         }
 
-        // if isCarouselWaitSet (initialized as false) is false and isShooterRunning is true set the
-        // carouselActivationWaitTime to the current time + CAROUSEL_ACTIVATION_TIME (set to 1000),
-        // and set isCarouselWaitSet to true, ensuring the activation time is only set once per toggle
-        if (!isCarouselWaitSet && isShooterRunning) {
-            // set carousel wait time to now + 1000 milliseconds (value of constant
-            // CAROUSEL_ACTIVATION_TIME) and set isCarouselWaitSet to true
-            carouselActivationWaitTime = time.milliseconds() + CAROUSEL_ACTIVATION_TIME;
-            isCarouselWaitSet = true;
-        }
+        if (gamepad1.right_bumper) {
+            // if driver presses right bumper, turn on carousel (used just in case rings get stuck in position)
+            if (gamepad1.right_bumper) {
+                carousel.set(CAROUSEL_SHOOTING_POWER);
+            } else {
+                carousel.set(0);
+            }
+            isCarouselManuallyStopped = true;
+        } else if (!isCarouselManuallyStopped) {
+            // if isCarouselWaitSet (initialized as false) is false and isShooterRunning is true set the
+            // carouselActivationWaitTime to the current time + CAROUSEL_ACTIVATION_TIME (set to 1000),
+            // and set isCarouselWaitSet to true, ensuring the activation time is only set once per toggle
+            if (!isCarouselWaitSet && isShooterRunning) {
+                // set carousel wait time to now + 1000 milliseconds (value of constant
+                // CAROUSEL_ACTIVATION_TIME) and set isCarouselWaitSet to true
+                carouselActivationWaitTime = time.milliseconds() + CAROUSEL_ACTIVATION_TIME;
+                isCarouselWaitSet = true;
+            }
 
-        // if the wait time has been completed (time is greater than the wait time) and the wait time
-        // has already been set, run carousel
-        if(time.milliseconds() > carouselActivationWaitTime && isCarouselWaitSet) {
-            // run carousel
-            carousel.set(CAROUSEL_POWER);
+            // if the wait time has been completed (time is greater than the wait time) and the wait time
+            // has already been set, run carousel
+            if (time.milliseconds() > carouselActivationWaitTime && isCarouselWaitSet) {
+                // run carousel
+                carousel.set(CAROUSEL_SHOOTING_POWER);
+                ringCount = 0;
+            }
         }
-
         // set prevLB to the driver's left bumper
         prevLB = gamepad1.left_bumper;
     }
@@ -133,85 +145,86 @@ public class ShooterIntakeSensor extends OpMode {
         } else {
             intake.set(0);
         }
-
-        // if driver presses right bumper, turn on spin (allowing rings to be pulled up before shooting)
-        if (gamepad1.right_bumper) {
-            carousel.set(CAROUSEL_POWER);
-        } else {
-            carousel.set(0);
-        }
     }
 
+
     // declare threshold for color sensor data
-    double alphaThreshold = 300;
+    double alphaThreshold = 250;
     // check whether or not carousel is running, ensures that carousel turns off at the correct time
     boolean isCarouselRunning = false;
     // keeps track of how many times the color sensor hasn't seen a ring
-    int notRing = 0;
+    boolean notRing = false;
     // keep track of rings indexed
     int ringCount = 0;
+
+    boolean prevY = false;
+    boolean isCarouselOn = false;
     // autonomously index rings as they're pulled in by the intake
     public void index() {
-        // check if driver's a is pressed. if it is, begin indexing
-        if(gamepad1.a) {
-            // if there are 0, 1, or 2 rings currently indexed, turn carousel off after the color
-            // sensor doesn't see the ring a second time
-            // if there are 3 rings indexed/pulled in by the intake, turn carousel off after the
-            // second time the color sensor sees the ring
-            if (ringCount < 3) {
-                // if the ring is seen and the carousel is not yet running, turn on the carousel and
-                // set isCarouselRunning to true
-                // if the ring is not seen and it's the first time it hasn't seen a ring, increment
-                // notRing
-                // if the ring is not seen an d it's the second time it hasn't seen the ring, turn
-                // off carousel, reset notRing, increment ringCount, and reset isCarouselRunning
-                // if all else fails, stop carousel
-                if (colorSensor.alpha() > alphaThreshold && !isCarouselRunning) {
-                    // turn on carousel and set isCarouselRunning to true
-                    carousel.set(.4);
-                    isCarouselRunning = true;
-                } else if (colorSensor.alpha() < alphaThreshold && notRing == 0 && isCarouselRunning) {
-                    // increment notRing
-                    carousel.set(.4);
-                    notRing = 1;
-                } else if (colorSensor.alpha() > alphaThreshold && isCarouselRunning) {
-                    carousel.set(.4);
-                } else if (colorSensor.alpha() < alphaThreshold && notRing == 1 && isCarouselRunning) {
-                    // turn off carousel, reset notRing, increment ringCount, and reset isCarouselRunning
-                    carousel.set(0);
-                    notRing = 0;
-                    ringCount++;
-                    isCarouselRunning = false;
-                } else {
-                    // stop carousel
-                    carousel.set(0);
-                }
-            } else if (ringCount == 3) {
-                // if the ring is seen and the carousel is not yet running, turn on the carousel and
-                // set isCarouselRunning to true
-                // if the ring is not seen and it's the first time it hasn't seen a ring, increment
-                // notRing
-                // if the ring is seen for a second time, turn off carousel, reset notRing, reset
-                // ringCount, and reset isCarouselRunning
-                // if all else fails, stop carousel
-                if (colorSensor.alpha() > alphaThreshold && !isCarouselRunning) {
-                    // turn on carousel and set isCarouselRunning to true
-                    carousel.set(CAROUSEL_POWER);
-                    isCarouselRunning = true;
-                } else if (colorSensor.alpha() < alphaThreshold && notRing == 0 && isCarouselRunning) {
-                    // increment not ring
-                    notRing = 1;
-                } else if (colorSensor.alpha() > alphaThreshold && notRing == 1 && isCarouselRunning) {
-                    // turn off carousel, reset notRing, reset ringCount, and reset isCarouselRunning
-                    carousel.set(0);
-                    notRing = 0;
-                    isCarouselRunning = false;
-                    ringCount = 0;
-                } else {
-                    // turn off carousel
-                    carousel.set(0);
-                }
-            }
+        if(gamepad1.y && !prevY) {
+            isCarouselOn = !isCarouselOn;
         }
+
+        if(!isCarouselOn) {
+            carousel.set(0);
+        }
+
+        shoot.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+
+        if(alphaThreshold < colorSensor.alpha() && !isCarouselRunning) {
+            carousel.set(CAROUSEL_INDEXING_POWER);
+            isCarouselRunning = true;
+            ringCount++;
+        }
+
+        if (alphaThreshold > colorSensor.alpha() && isCarouselRunning && !notRing) {
+            notRing = true;
+            carousel.set(CAROUSEL_INDEXING_POWER);
+        }
+
+        if(alphaThreshold < colorSensor.alpha() && notRing && ringCount < 3) {
+            carousel.set(CAROUSEL_INDEXING_POWER);
+        }
+
+        if (alphaThreshold < colorSensor.alpha() && notRing && ringCount == 3) {
+            carousel.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+            carousel.set(0);
+            isCarouselRunning = false;
+            notRing = !notRing;
+        }
+
+        if (alphaThreshold > colorSensor.alpha() && notRing) {
+            carousel.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+            carousel.set(0);
+            isCarouselRunning = false;
+            notRing = !notRing;
+        }
+
+        prevY = gamepad1.y;
+        /*if(colorSensor.alpha() >= alphaThreshold  && !isCarouselRunning) {
+            ringCount++;
+            carousel.set(CAROUSEL_INDEXING_POWER);
+            isCarouselRunning = true;
+        }
+        if(colorSensor.alpha() <= alphaThreshold && isCarouselRunning && notRing == 0) {
+            carousel.set(CAROUSEL_INDEXING_POWER);
+            notRing++;
+        }
+        if(colorSensor.alpha() >= alphaThreshold && isCarouselRunning && ringCount != 3) {
+            carousel.set(CAROUSEL_INDEXING_POWER);
+        }
+
+        if(colorSensor.alpha() >= alphaThreshold && isCarouselRunning && ringCount == 3) {
+            carousel.set(0);
+            notRing = 0;
+            isCarouselRunning = false;
+        }
+
+        if(colorSensor.alpha() <= alphaThreshold && notRing == 1 && isCarouselRunning &&
+                ringCount != 3) {
+            notRing = 0;
+            isCarouselRunning = false;
+            carousel.set(0);
+        } */
     }
 }
